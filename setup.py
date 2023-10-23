3caa7c682aa55fd3a3f63841f2f6195a945b893f
esantizooctagt <esantizo@octagt.com>
2023-10-23 03:27:51 -0600

    update

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='easyrpc',  
-     version='BUILD_VERSION',
+     name='easyrpc_v2',  
+     version='2.0.4',
      packages=setuptools.find_packages(include=['easyrpc', 'easyrpc.tools'], exclude=['build']),
      author="Joshua Jamison",
      author_email="joshjamison1@gmail.com",
@@ -17,5 +17,5 @@ setuptools.setup(
          "Operating System :: OS Independent",
      ],
      python_requires='>=3.7, <4',   
-     install_requires=['makefun==1.9.5', 'PyJWT==2.0.0', 'fastapi', 'uvicorn', 'websockets', 'aiohttp'],
+     install_requires=['makefun', 'PyJWT', 'fastapi', 'uvicorn', 'websockets', 'aiohttp'],
  )
